71eac0ed09c5c38fbccd5aa587ab0186ffd50b34
Ramaswamy Narayanan <ramaswamy.bn@perficient.com>
2019-11-14 17:07:34 +0530

    added the navigation items and navigation item

---
+++ burder-builder/src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from "./NavigationItem.module.css";
+
+const NavigationItem = props => (
+  <li className={classes.NavigationItem}>
+    <a className={props.active ? classes.active : null} href={props.link}>
+      {props.children}
+    </a>
+  </li>
+);
+
+export default NavigationItem;
