194d78f01aede399b962318e2dbb11a86759ff8e
Ramaswamy Narayanan <ramaswamy.bn@perficient.com>
2019-11-17 15:25:29 +0530

    added SideDrawer

--- burder-builder/src/components/Layout/Layout.js
+++ burder-builder/src/components/Layout/Layout.js
@@ -2,10 +2,13 @@ import React from "react";
 import Auxillary from "../../hoc/Auxillary";
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+
 
 const Layout = props => (
   <Auxillary>
     <Toolbar />
+    <SideDrawer />
     <main className={classes.Content}>{props.children}</main>
   </Auxillary>
 );
