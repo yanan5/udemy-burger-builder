2b57c675d96ebd8991bb9c31271c33a00edc8662
Ramaswamy Narayanan <ramaswamy.bn@perficient.com>
2020-03-12 21:30:21 +0530

    Added interceptors

--- http--01-starting-setup/src/index.js
+++ http--01-starting-setup/src/index.js
@@ -3,6 +3,30 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
+import axios from "axios";
 
+const requestInterceptor = axios.interceptors.request.use(
+  request => {
+    console.log("request",request);
+    return request;
+  },
+  err => {
+    console.log(err);
+    return Promise.reject(err);
+  }
+);
+// axios.interceptors.request.eject(requestInterceptor);
+
+const responseInterceptor = axios.interceptors.response.use(
+  response => {
+    console.log("response", response);
+    return response;
+  },
+  err => {
+    console.log(err);
+    return Promise.reject(err);
+  }
+);
+// axios.interceptors.response.eject(responseInterceptor);
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
