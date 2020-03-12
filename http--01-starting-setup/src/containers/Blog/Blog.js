587a0a526175153e9a053928a31542954ee26b56
Ramaswamy Narayanan <ramaswamy.bn@perficient.com>
2020-03-12 15:25:13 +0530

    added axios and made a get request

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -1,28 +1,33 @@
-import React, { Component } from 'react';
-
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
-import './Blog.css';
+import React, { Component } from "react";
+import axios from "axios";
+import Post from "../../components/Post/Post";
+import FullPost from "../../components/FullPost/FullPost";
+import NewPost from "../../components/NewPost/NewPost";
+import "./Blog.css";
 
 class Blog extends Component {
-    render () {
-        return (
-            <div>
-                <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
-                </section>
-                <section>
-                    <FullPost />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
-        );
-    }
+  componentDidMount() {
+    axios.get("https://jsonplaceholder.typicode.com/posts")
+    .then(res => res.data)
+    .then(data => console.log(data));
+  }
+  render() {
+    return (
+      <div>
+        <section className="Posts">
+          <Post />
+          <Post />
+          <Post />
+        </section>
+        <section>
+          <FullPost />
+        </section>
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Blog;
