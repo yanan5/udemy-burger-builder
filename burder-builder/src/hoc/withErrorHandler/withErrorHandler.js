3def05ea722936d971c2a495398060a3d9cbdb64
Ramaswamy Narayanan <ramaswamy.bn@perficient.com>
2020-03-13 12:57:32 +0530

    Handling error using higher order components

---
+++ burder-builder/src/hoc/withErrorHandler/withErrorHandler.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Modal from "../../components/UI/Modal/Modal";
+import Aux from "../Auxillary/Auxillary";
+const withErrorHandler = (Component, axiosInstance) =>
+  class extends React.Component {
+    state = {
+      error: false
+    };
+    componentDidMount() {
+      axiosInstance.interceptors.request.use(req => {
+        this.setState({ error: false });
+        return req;
+      });
+      axiosInstance.interceptors.response.use(
+        res => res,
+        error => this.setState({ error })
+      );
+    }
+    closeModal = () => this.setState({ error: false });
+    render() {
+      return (
+        <Aux>
+          <Modal show={Boolean(this.state.error)} modalClosed={this.closeModal}>
+            {this.state.error ? this.state.error.message : null}
+          </Modal>
+          <Component {...this.props} />
+        </Aux>
+      );
+    }
+  };
+
+export default withErrorHandler;
