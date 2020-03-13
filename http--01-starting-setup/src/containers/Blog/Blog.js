0e332f4e5a10ee58aed6cfdb0fc018ab5b5d355a
Ramaswamy Narayanan <ramaswamy.bn@perficient.com>
2020-03-13 18:35:00 +0530

    Setup the router package

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -1,8 +1,5 @@
 import React, { Component } from "react";
 import axios from "../../axios";
-import Post from "../../components/Post/Post";
-import FullPost from "../../components/FullPost/FullPost";
-import NewPost from "../../components/NewPost/NewPost";
 import "./Blog.css";
 
 class Blog extends Component {
@@ -41,12 +38,6 @@ class Blog extends Component {
           </nav>
         </header>
         <section className="Posts">{posts}</section>
-        <section>
-          <FullPost id={this.state.selectedPostId} />
-        </section>
-        <section>
-          <NewPost />
-        </section>
       </div>
     );
   }
