4ad9d85c00d6850eeed9b18a2b59478e60d22dff
Ramaswamy Narayanan <ramaswamy.bn@perficient.com>
2020-03-20 14:28:35 +0530

    Added back navigation and contact-data url

--- burder-builder/src/containers/Checkout/Checkout.js
+++ burder-builder/src/containers/Checkout/Checkout.js
@@ -9,10 +9,19 @@ class Checkout extends React.Component {
       bacon: 1
     }
   }
+  checkoutCancelledHandler = () => {
+    this.props.history.goBack();
+  }
+  checkoutContinuedHandler = () => {
+    this.props.history.replace('/checkout/contact-data');
+  }
   render() {
     return (
       <div>
-        <CheckoutSummary ingredients={this.state.ingredients} />
+        <CheckoutSummary
+          checkoutCancelled={this.checkoutCancelledHandler}
+          checkoutContinued={this.checkoutContinuedHandler}
+          ingredients={this.state.ingredients} />
       </div>
     )
   }
