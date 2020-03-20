75edb58a7ed1c32b651ddb802ef414e446727ece
Ramaswamy Narayanan <ramaswamy.bn@perficient.com>
2020-03-20 10:43:24 +0530

    removed the console log

--- routing--assignment-problem/src/containers/Course/Course.js
+++ routing--assignment-problem/src/containers/Course/Course.js
@@ -3,7 +3,6 @@ class Course extends Component {
   render() {
     const search = this.props.location.search.substr(0);
     const value = search.split("=")[1]
-     console.log(this.props)
     return (
       <div>
         <h1>{decodeURIComponent(value)}</h1>
