0cfb981e6380176e9ab0269cfe5f78dd81ef2858
Ramaswamy Narayanan <ramaswamy.bn@perficient.com>
2020-03-31 12:03:06 +0530

    Added Orders page

--- burder-builder/src/App.js
+++ burder-builder/src/App.js
@@ -3,12 +3,14 @@ import { Route } from "react-router-dom";
 import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Checkout from "./containers/Checkout/Checkout";
+import Orders from "./containers/Orders/Orders";
 
 function App() {
   return (
     <div>
       <Layout>
         <Route exact path="/" component={BurgerBuilder} />
+        <Route exact path="/orders" component={Orders} />
         <Route path="/checkout" component={Checkout} />
       </Layout>
     </div>
