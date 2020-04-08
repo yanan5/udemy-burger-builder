5a9da7b59201a57e7bdbf180ee33151af9706101
Ramaswamy Narayanan <ramaswamy.bn@perficient.com>
2020-04-08 10:03:37 +0530

    gaurded the routes

--- burder-builder/src/App.js
+++ burder-builder/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, withRouter } from "react-router-dom";
+import { Route, withRouter, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
@@ -11,24 +11,39 @@ import { authCheckState } from "../src/actions";
 
 class App extends React.Component {
   componentDidMount() {
-    this.props.authCheckState()
+    this.props.authCheckState();
   }
   render() {
-    return (
-      <div>
-        <Layout>
+    const { isAuthenticated } = this.props;
+    let routes = (
+      <React.Fragment>
+        <Route exact path="/" component={BurgerBuilder} />
+        <Route path="/auth" component={Auth} />
+        <Redirect to="/" />
+      </React.Fragment>
+    );
+    if (isAuthenticated) {
+      routes = (
+        <React.Fragment>
           <Route exact path="/" component={BurgerBuilder} />
           <Route exact path="/orders" component={Orders} />
           <Route path="/checkout" component={Checkout} />
-          <Route path="/auth" component={Auth} />
           <Route path="/logout" component={Logout} />
-        </Layout>
+          <Redirect to="/" />
+        </React.Fragment>
+      );
+    }
+    return (
+      <div>
+        <Layout>{routes}</Layout>
       </div>
     );
   }
 }
-
+const mapStateToProps = ({ auth: { token } }) => ({
+  isAuthenticated: token !== null,
+});
 const mapDispatchToProps = {
   authCheckState,
 };
-export default withRouter(connect(null, mapDispatchToProps)(App));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
