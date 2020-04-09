5a4b232e626860d6899e1b79260d0f38581447e5
Ramaswamy Narayanan <ramaswamy.bn@perficient.com>
2020-04-09 14:36:48 +0530

    Added getInitialProps

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,27 @@
-import Link from 'next/link';
-import Router from 'next/router';
+import React, { Component } from "react";
 
-const Index = () => (
-  <div>
-    <h1>Hello Next.js</h1>
-    <Link href="/auth">
-      <a>Auth</a>
-    </Link>
-    <button onClick={() => Router.push("/auth")}>Click to got to Auth</button>
-  </div>
-);
+import Link from "next/link";
+import Router from "next/router";
+
+class Index extends Component {
+  static async getInitialProps(context) {
+    console.log(context);
+    return {appName : "Super App"}
+  }
+  render() {
+    return (
+      <div>
+        <h1>Hello Next.js</h1>
+        <h4>{this.props.appName}</h4>
+        <Link href="/auth">
+          <a>Auth</a>
+        </Link>
+        <button onClick={() => Router.push("/auth")}>
+          Click to got to Auth
+        </button>
+      </div>
+    );
+  }
+}
 
 export default Index;
