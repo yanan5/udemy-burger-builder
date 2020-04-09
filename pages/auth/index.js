e99f6da6fc4efb25c96d5f43f0d9bbc4e529f59e
Ramaswamy Narayanan <ramaswamy.bn@perficient.com>
2020-04-09 14:20:57 +0530

    added components and consumed it added styled jsx

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,8 +1,9 @@
-import Link from 'next/link';
-
+import Link from "next/link";
+import User from "../../components/User";
 const Auth = () => (
   <div>
     <h2>Auth Page</h2>
+    <User name="Shobana" age="32" />
     <Link href="/">
       <a>Go Back</a>
     </Link>
