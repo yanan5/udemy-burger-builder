a345afed9b2bd073baabed76341ca8f76797e589
Ramaswamy Narayanan <ramaswamy.bn@perficient.com>
2020-04-16 12:04:12 +0530

    used context api instead of redux store

--- replace-redux-01-starting-project/src/components/Products/ProductItem.js
+++ replace-redux-01-starting-project/src/components/Products/ProductItem.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
+import React,{ useContext }  from 'react';
 
 import Card from '../UI/Card';
 import './ProductItem.css';
-import { toggleFav } from '../../store/actions/products';
+import {ProductsContext} from '../../context/products-context';
 
 const ProductItem = props => {
-  const dispatch = useDispatch();
-
+  const toggleFavorite = useContext(ProductsContext).toggleFavorite
   const toggleFavHandler = () => {
-    dispatch(toggleFav(props.id));
+    toggleFavorite(props.id);
   };
 
   return (
